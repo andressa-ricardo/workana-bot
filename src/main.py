04cfe6bcf47edfdd3d9cb6b3a83a073754367000
andressa-ricardo <andressaricardo30@gmail.com>
2024-09-27 16:13:21 -0300

    feat: login workana

---
+++ src/main.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+import time
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def login_workana():
+    username = os.getenv('WORKANA_EMAIL')
+    password = os.getenv('WORKANA_PASSWORD')
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get(os.getenv('URL_WORKANA'))
+    time.sleep(5)
+    
+    try:
+        cookies = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
+        cookies.click()
+        print("Aceitou os cookies!")
+    except NoSuchElementException:
+        pass
+    user_input = driver.find_element(By.NAME, 'email')
+    pass_input = driver.find_element(By.NAME, 'password')
+    user_input.send_keys(username)
+    pass_input.send_keys(password)
+    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
+    login_button.click()
+    print("Logou no workana!")
+    time.sleep(5)
+    return driver
+
+def main():
+    driver = login_workana()
+    driver.quit()
+if __name__ == "__main__":
+    main()
