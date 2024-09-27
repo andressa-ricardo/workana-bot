e3757e3ec35b944436e7f135564f6e7d584add7c
andressa-ricardo <andressaricardo30@gmail.com>
2024-09-27 16:49:52 -0300

    refactor: changing the structure

--- src/main.py
+++ src/main.py
@@ -1,41 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.keys import Keys
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import NoSuchElementException
-import time
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-def login_workana():
-    username = os.getenv('WORKANA_EMAIL')
-    password = os.getenv('WORKANA_PASSWORD')
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-    driver.maximize_window()
-    driver.get(os.getenv('URL_WORKANA'))
-    time.sleep(5)
-    
-    try:
-        cookies = driver.find_element(By.ID, 'onetrust-accept-btn-handler')
-        cookies.click()
-        print("Aceitou os cookies!")
-    except NoSuchElementException:
-        pass
-    user_input = driver.find_element(By.NAME, 'email')
-    pass_input = driver.find_element(By.NAME, 'password')
-    user_input.send_keys(username)
-    pass_input.send_keys(password)
-    login_button = driver.find_element(By.XPATH, "//button[@type='submit']")
-    login_button.click()
-    print("Logou no workana!")
-    time.sleep(5)
-    return driver
+from bot.workana_bot import exeutar_bot
 
 def main():
-    driver = login_workana()
-    driver.quit()
+    exeutar_bot()
+
 if __name__ == "__main__":
     main()
